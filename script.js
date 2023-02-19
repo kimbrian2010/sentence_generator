37b78fb763ce5268cae53e27b8e14e13bdd9ec61
Ackim Brian <kimbrian2010@gmail.com>
2023-02-19 17:53:58 +0200

    Added JavaScript

---
+++ script.js
@@ -0,0 +1,31 @@
+const btn = document.getElementById("btn")
+const jokeParagraph = document.getElementById("joke")
+
+
+const api_key = "bK+3OFwIMgg2msZgZ62ngA==B3hQoBXIEHKywEq8"
+
+const options = {
+    method: "GET",
+    headers: {
+        "X-Api-Key": api_key
+    }
+}
+
+const api_url = "https://api.api-ninjas.com/v1/chucknorris?limit=1"
+
+async function getSentence() {
+    jokeParagraph.inert = "Fetching joke..." /*msg to appear we wait for the joke to be fetched */
+    btn.disabled = true
+    btn.innerText = "Upadating joke"
+
+    const response = await fetch(api_url, options)
+    const data = await response.json()
+
+    btn.disabled = false
+    btn.innerText = "Get New Joke"
+
+    jokeParagraph.innerText = data.joke
+    console.log(data)
+}
+
+btn.addEventListener("click", getSentence)
